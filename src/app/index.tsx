b9f71734302e862f4420f14d3672c58df54b213d
Aaddityaaa <adityau5090@gmail.com>
2026-07-11 23:40:08 +0530

    initialize project and add uniwind

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-red-300">
+      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
